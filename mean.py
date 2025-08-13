8e0673435bf213c3dd566688e0a28a3eb92f31cf
Brawlstars902 <aparna.rautela@gmail.com>
2025-08-13 10:24:12 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,8 @@
+print('Please enter five numbers to find the mean(decimals are allowed).')
+x=float(input('Number1:'))
+y=float(input('Number2:'))
+z=float(input('Number3:'))
+a=float(input('Number4:'))
+b=float(input('Number5:'))
+mean=(x+y+z+a+b)/5
+print('The average of these numbers are',mean,'.')
