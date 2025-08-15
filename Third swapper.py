ff40be650ea6159357fb14eed0abbc6d25c4cf3b
Brawlstars902 <aparna.rautela@gmail.com>
2025-08-15 14:08:42 +0530

    Add files via upload

---
+++ Third swapper.py
@@ -0,0 +1,13 @@
+x=input('Please enter a number from 1-10. This is variable x:')
+y=input('Please enter another number from 1-10. This is variable y:')
+z=input('Please enter a third number from 1-10. This is variable z:')
+a=input('Please enter a number from 100-1000. This is variable a:')
+b=input('Please enter another number from 100-1000. This is variable b:')
+c=input('Please enter a third number from 100-1000. This is variable c:')
+x=a
+y=b
+z=c
+a=x
+b=y
+c=z
+print('The variables x,y and z are now',x,',',y,'and',z,'because the variable x has swapped with a, the variable y has swapped with b and the variable z has swapped with c.')
